f9ece59469e4e29689a675afec3e262b9dd43ca4
Dongseon Yang <78548833+dongseon0@users.noreply.github.com>
2023-11-08 10:21:53 +0900

    go

---
+++ .bash_profile
@@ -0,0 +1,2 @@
+export MVN=${HOME}/Users/yangdongseon/Documents/apache-maven-3.9.5
+export PATH=$PATH:${MVN}/bin
---
+++ src/test/java/medongseon/inflearnrestapi/InflearnRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package medongseon.inflearnrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
